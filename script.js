766e471ee3d674b9fa01ddced8a392ae03d80056
Yauheny Bychkou <375445433672@yandex.by>
2021-08-15 17:30:33 +0300

    ready100percent

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ const todoСontrol = document.querySelector(".todo-control"),
   todoList = document.querySelector(".todo-list"),
   todoCompleted = document.querySelector(".todo-completed");
 
-let todoData = [];
+let todoData = localStorage.getItem("todoData") ? JSON.parse(localStorage.getItem("todoData")) : [];
 
 const render = function () {
-  localStorage.setItem("todoData", JSON.stringify(todoData));
-  todoData = JSON.parse(localStorage.getItem("todoData"));
   todoList.textContent = "";
   todoCompleted.textContent = "";
   todoData.forEach(function (item) {
     const li = document.createElement("li");
+
     li.classList.add("todo-item");
     li.innerHTML =
       '<span class="text-todo">' +
@@ -22,22 +21,29 @@ const render = function () {
       '<button class="todo-remove"></button>' +
       '<button class="todo-complete"></button>' +
       "</div>";
+
     todoList.append(li);
+
     if (item.completed) {
       todoCompleted.append(li);
     } else {
       todoList.append(li);
     }
+
     const btnTodoDelete = li.querySelector(".todo-remove");
+
     btnTodoDelete.addEventListener("click", function () {
-      li.remove();
-      todoData.splice(item.value);
+      const index = todoData.indexOf(item);
+      todoData.splice(index, 1);
+      render();
     });
+
     const btnTodoComplete = li.querySelector(".todo-complete");
     btnTodoComplete.addEventListener("click", function () {
       item.completed = !item.completed;
       render();
     });
+    localStorage.setItem("todoData", JSON.stringify(todoData));
   });
 };
 
